ee4cd2e64ff403ea2de9a0c987940735100ec6d0
Alex Aaron <alexfrancisaaron@gmail.com>
2025-04-23 00:14:43 +0000

    created watchList array

---
+++ index.js
@@ -0,0 +1,63 @@
+const watchList = [
+  {
+    title: 'Come and See',
+    director: 'Elem Klimov',
+    year: 1985,
+    genreTags: ['Drama', 'World War 2'],
+    streaming: [
+      {
+        platform: 'Criterion Channel',
+        paid: true,
+      }
+    ]
+  },
+  {
+    title: 'His Girl Friday',
+    director: 'Howard Hawks',
+    year: 1940,
+    genreTags: ['Comedy'],
+    streaming: [
+      {
+        platform: 'Tubi',
+        paid: false,
+      },
+      {
+        platform: 'Max',
+        paid: True
+      }
+    ]
+  },
+  {
+    title: 'Bringing Up Baby',
+    director: 'Howard Hawks',
+    year: 1938,
+    genreTags: ['Comedy'],
+    streaming: [
+      {
+        platform: 'Tubi',
+        paid: false
+      },
+      {
+        platform: 'Criterion Channel',
+        paid: true
+      }
+    ]
+  },
+  {
+    title: 'Shoah',
+    director: 'Claude Lanzmann',
+    year: 1985,
+    genreTags: ['Documentary', 'World War 2'],
+    streaming: [
+      {
+        platform: 'Max',
+        paid: true
+      },
+      {
+        platform: 'Mubi',
+        paid: true
+      }
+    ]
+  }
+];
+
